8ec23702843a3fc2fe057c66bf4e2e85dda33aaf
cyw <weems1995@hotmail.com>
2018-03-20 00:12:14 +0800

    Record finished?

--- records.py
+++ records.py
@@ -34,4 +34,38 @@ class Record(object):
     def __repr__(self):
         return '<Record {}>'.format(self.export('json')[1:-1])
 
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.values()[key]
+
+        if key in self.keys():
+            i = self.keys().index(key)
+            if self.keys().count(key) > 1:
+                raise KeyError("Record contains multiple '{}' fields".format(key))
+            return self.values()[i]
+
+        return KeyError("Record contains multiple no '{}' fields")
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError as e:
+            return AttributeError(e)
+
+    def __dir__(self):
+        standard = dir(super(Record, self))
+        return sorted(standard + [str(k) for k in self.keys()])
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def as_dict(self, ordered=False):
+        items = zip(self.keys(), self.values())
+
+        return OrderedDict(items) if ordered else dict(items)
+
+
 
