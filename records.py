69045ec212d728f6b8328132e08dd209eeef4c14
cyw <weems1995@hotmail.com>
2018-04-13 10:23:59 +0800

    RecordCollection

--- records.py
+++ records.py
@@ -92,9 +92,79 @@ class RecordCollection:
         return '<RecordCollection size={} pending={}>'.format(len(self), self.pending)
 
     def __iter__(self):
+        i = 0
+        while True:
+            if i < len(self):
+                yield self[i]
+            else:
+                try:
+                    yield next(self)
+                except StopIteration:
+                    return
+            i += 1
+
+    def next(self):
+        return self.__next__()
+
+    def __next__(self):
+        try:
+            nextrow = next(self._rows)
+            self._all_rows.append(nextrow)
+            return nextrow
+        except StopIteration:
+            self.pending = False
+            raise StopIteration("RecordCollection contains no more rows.")
 
     def __len__(self):
         return len(self._all_rows)
+
+    def __getitem__(self, key):
+        is_int = isinstance(int, key)
+
+        if is_int:
+            key = slice(key, key + 1)
+
+        while len(self) < key.stop or key.stop is None:
+            try:
+                next(self)
+            except StopIteration:
+                break
+
+        rows = self._all_rows[key]
+        if is_int:
+            return rows[0]
+        return RecordCollection(iter(rows))
+
+    def __len__(self):
+        return len(self._all_rows)
+
+    def export(self, format, **kwargs):
+        return self.dataset.export(format, **kwargs)
+
+    @property
+    def dataset(self):
+        data = tablib.Dataset()
+        if len(list(self)) == 0:
+            return data
+
+        first = self[0]
+
+        data.headers = first.keys()
+        for row in self.all():
+            row = _reduce_datetimes(row.values())
+            data.append(row)
+        return data
+
+    def all(self, as_dict=False, as_ordereddict=False):
+        rows = list(self)
+
+        if as_dict:
+            return [r.as_dict() for r in rows]
+        elif as_ordereddict:
+            return [r.as_dict(ordered=True) for r in rows]
+
+    def as_dict(self, ordered=False):
+        return self.all(as_dict=not ordered, as_ordereddict=ordered)
 
 
 def _reduce_datetimes(row):
